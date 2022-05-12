7558deb6805758f51a9207b5415e6f5d935e2c3e
Peter Schuler <peter.schuler@rabobank.nl>
2022-05-12 09:22:40 +0200

    Initial Spring boot project setup.

---
+++ src/main/java/org/demo/microservice/schoppingcart/schoppingcartapp/SchoppingcartappApplication.java
@@ -0,0 +1,13 @@
+package org.demo.microservice.schoppingcart.schoppingcartapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoppingcartappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoppingcartappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/demo/microservice/schoppingcart/schoppingcartapp/SchoppingcartappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.microservice.schoppingcart.schoppingcartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoppingcartappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
